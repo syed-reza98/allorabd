1d6c64f878601a5d0249bd16443ebc72b9a45de2
Syed Salman Reza <syed.reza@northsouth.edu>
2023-12-11 01:44:12 +0600

    Laravel + Nuxt

---
+++ allora/breeze-nuxt/nuxt.config.ts
@@ -0,0 +1,24 @@
+import { defineNuxtConfig } from "nuxt/config";
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  modules: ["@nuxt/ui"], //"@nuxtjs/tailwindcss"
+  runtimeConfig: {
+    public: {
+      backendUrl: "http://localhost:8000",
+      frontendUrl: "http://localhost:3000",
+    },
+  },
+  imports: {
+    dirs: ["./utils"],
+  },
+  experimental: {
+    asyncContext: true,
+    },
+
+    devtools: {
+    timeline: {
+        enabled: true
+    }
+  }
+});
