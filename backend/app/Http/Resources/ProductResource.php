ce428b1ea142237e450e22a453edb70ece4c342f
Syed Salman Reza <71028588+syed-reza98@users.noreply.github.com>
2023-12-26 17:58:30 +0600

    up

--- backend/app/Http/Resources/ProductResource.php
+++ backend/app/Http/Resources/ProductResource.php
@@ -26,16 +26,10 @@ class ProductResource extends JsonResource
         // return parent::toArray($request);
         return [
             'id' => $this->id,
-            'category_id' => $this->category_id,
-            'sku' => $this->sku,
             'name' => $this->name,
-            'slug' => $this->slug,
             'image_url' => $this->image_url,
-            'is_visible' => $this->is_visible,
             'description' => $this->description,
             'price' => $this->price,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
         ];
     }
 }
